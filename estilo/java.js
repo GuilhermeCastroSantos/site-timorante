11eef123a934b990fe6e1065c984d64e3aba1a1a
GuilhermeCastroSantos <ghuilherme29@gmail.com>
2025-01-24 12:26:29 -0300

    atualização

--- estilo/java.js
+++ estilo/java.js
@@ -3,9 +3,12 @@ document.getElementById('newsForm').addEventListener('submit', function(event)
     event.preventDefault(); // impede o reload da página
 
     const title = document.getElementById('title').Value;
+
     const content = document.getElementById('content').Value;
-    const.getElementById('newsContainer');
-    const newsContainer = document.createElement('div');
+
+    const newsContainer = getElementById('newsContainer');
+
+    const newArticle = document.createElement('div');
     newArticle.innerHTML = '<h3>${title}</h3><p>${content}</p>';
     newsContainer.appendChild(newArticle); // Limpa os campos após o envio
 
