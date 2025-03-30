b62843bf7f8043511dc3520f2f635e1d473fc761
GuilhermeCastroSantos <ghuilherme29@gmail.com>
2025-03-30 20:05:57 -0300

    novo
    novo

--- JavaScript/estilo.js
+++ JavaScript/estilo.js
@@ -1,20 +1,3 @@
-document.getElementById('newsForm').addEventListener('submit', function(event)
-{
-    event.preventDefault(); // impede o reload da página
-
-    const title = document.getElementById('title').Value;
-
-    const content = document.getElementById('content').Value;
-
-    const newsContainer = getElementById('newsContainer');
-
-    const newArticle = document.createElement('div');
-    newArticle.innerHTML = '<h3>${title}</h3><p>${content}</p>';
-    newsContainer.appendChild(newArticle); // Limpa os campos após o envio
-
-    document.getElementById('newsForm').reset();
-});
-
 function submitNews() {
     const title = document.getElementById('title').value;
     const content = document.getElementById('content').value;
